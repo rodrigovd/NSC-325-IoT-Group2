8fb70bcbe8d17125c631dfdb6770be760c76b409
Rodrigo <rodrigoviveros22@gmail.com>
2021-03-09 15:23:59 -0600

    Option to scan different interfaces

--- find_ip.py
+++ find_ip.py
@@ -1,18 +1,29 @@
 import netifaces
+
+print("Finding your Interface.....")
+list_of_interfaces = netifaces.interfaces()
+print(list_of_interfaces)
+
+def interface():
+     interf = input("Enter your interface:")
+     if interf in list_of_interfaces:
+          return interf
+     else:
+          print("Interface not found be sure to enter your selecction excatly")
+          return interface()
+
 def find_gateway():
-      Interfaces= netifaces.interfaces()
-      for inter in Interfaces:
-           if inter == "enp2s0":
-                temp_list = []
-                Addresses = netifaces.ifaddresses(inter)
-                gws = netifaces.gateways()
-                temp_list = list (gws['default'][netifaces.AF_INET])
-                count =0 
-                for item in temp_list:
-                      count +=1
-                      if count ==1:
-                           # print (item)
-                           return item
-                      else:
-                           pass
+          inter = interface()
+          temp_list = []
+          Addresses = netifaces.ifaddresses(inter)
+          gws = netifaces.gateways()
+          temp_list = list (gws['default'][netifaces.AF_INET])
+          count =0 
+          for item in temp_list:
+               count +=1
+               if count ==1:
+                    print (item)
+                    return item
+               else:
+                    pass
 find_gateway()
